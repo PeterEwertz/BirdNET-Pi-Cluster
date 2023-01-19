d7e7e767df970c7c8eb703f3b790013aeaa17d39
PeterEwertz <p.ewertz@gmx.de>
2023-01-19 17:36:59 +0000

    commit

--- server/configure.system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-SERVERDIR="birdnet-pi_cluster/server"
-
-cp $HOME/$SERVERDIR/etc/telegraf/telegraf.d/io_pg_birdnet-pi_cl.conf /etc/telegraf/telegraf.d/io_pg_birdnet-pi_cl.conf 
-#sudo service telegraf enable
-sudo service telegraf restart
