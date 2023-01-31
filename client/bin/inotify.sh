b978d3ce6146c474c14d8b2a65e743cc09a4314d
PeterEwertz <p.ewertz@gmx.de>
2023-02-01 00:22:06 +0100

    Anpassungen

--- client/bin/inotify.sh
+++ client/bin/inotify.sh
@@ -8,14 +8,14 @@ set -x
 [ -z $RECS_DIR ] && RECS_DIR=/tmp/ino  # Testconfiguration if not set
 [ -z $MQTT_BROKER ] && MQTT_BROKER="49.12.4.122"
 [ -z $TOPIC_PRE ] && TOPIC_PRE="birdnet_pi_cl"
-[ -z $BPC_PATH ] && BPC_PATH=/home/pew/Dokumente/Projekte/Birds/birdnet-pi_cluster/client/bin
+[ -z $BPC_PATH ] && BPC_PATH=/home/pew//birdnet_pi_cluster/client/bin
 
 [ -z $MAC_ADRESS ] && MAC_ADRESS=$($BPC_PATH/getMacAdress.sh)
 
 sox2json() {
 #  Format sox output to json
 	sox  $1 -n stat 2>&1  |\
-	awk -v anzahl=15 -v mac_adress=$MAC_ADRESS -F':' '
+	awk -v anzahl=14 -v mac_adress=$MAC_ADRESS -F':' '
 		function escape_colon(s) { gsub(/:/, "\\:" s); return s }
 		BEGIN { 
 			printf ("{\"mac_adress\":\"%s\",", escape_colon(mac_adress)) 
@@ -28,7 +28,7 @@ sox2json() {
 			gsub(/ +/, "_", $1)	# replace " " with "_"
 			gsub(/\(|\)/, "", $1)	# replace "(" and ")" with nothing
 			if ( NR != anzahl ) {
-				printf ("\"%s\":%s,", trim($1), trim($2))
+				printf ("\"%d%s\":%s,", NR, trim($1), trim($2))
 			} else {
 				printf ("\"%s\":%s",  trim($1), trim($2))
 			}
@@ -37,7 +37,7 @@ sox2json() {
 	' 
 }
 
-inotifywait -m $RECS_DIR -e create |
+inotifywait -m $PROCESSED -e create,moved_to --exclude '.*.csv' |
     while read DIR ACTION FILE; do
 	if [ -f $DIR$FILE ]; then
         	#echo "The FILE '$FILE' appeared in directory '$DIR' via '$ACTION'"
