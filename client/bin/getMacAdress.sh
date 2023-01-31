a4070132f6145585a8903c2ced47facc2611f38b
Peter Ewertz <P.Ewertz@gmx.de>
2023-01-31 16:30:48 +0100

    ac für arm + x86

--- client/bin/getMacAdress.sh
+++ client/bin/getMacAdress.sh
@@ -1,1 +1,10 @@
-echo "ff:ff:ff:ff:ff:ff"
+#!/usr/bin/env bash
+
+#set -x
+if [ -d /sys/class/net/eth0 ] ; then
+	echo $(cat /sys/class/net/eth0/address)
+elif [ -d /sys/class/net/eno1 ] ; then
+	echo $(cat /sys/class/net/eno1/address)
+else 	
+	echo "ff:ff:ff:ff:ff:ff"
+fi
