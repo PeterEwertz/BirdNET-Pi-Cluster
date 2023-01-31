ebe8fae216726bf0abca92b7c65dc480d52fb585
Peter Ewertz <P.Ewertz@gmx.de>
2023-01-31 16:26:32 +0100

    sox-n => MQTT

--- client/bin/inotify.sh
+++ client/bin/inotify.sh
@@ -4,26 +4,37 @@
 
 set -x
 [ -f /etc/birdnet/birdnet.conf ] && source /etc/birdnet/birdnet.conf
-[ -z $RECS_DIR ] && RECS_DIR=/tmo/ino  # Testconfiguration if not set
+[ -f /etc/birdnet/birdnet-pi-cluster.conf ] && source /etc/birdnet/birdnet-pi-cluster.conf
+[ -z $RECS_DIR ] && RECS_DIR=/tmp/ino  # Testconfiguration if not set
+[ -z $MQTT_BROKER ] && MQTT_BROKER="49.12.4.122"
+[ -z $TOPIC_PRE ] && TOPIC_PRE="birdnet_pi_cl"
+[ -z $BPC_PATH ] && BPC_PATH=/home/pew/Dokumente/Projekte/Birds/birdnet-pi_cluster/client/bin
+
+[ -z $MAC_ADRESS ] && MAC_ADRESS=$($BPC_PATH/getMacAdress.sh)
 
 sox2json() {
 #  Format sox output to json
 	sox  $1 -n stat 2>&1  |\
-	awk -v anzahl=15 -F':' '
-		BEGIN { printf ("%s", "{") }
+	awk -v anzahl=15 -v mac_adress=$MAC_ADRESS -F':' '
+		function escape_colon(s) { gsub(/:/, "\\:" s); return s }
+		BEGIN { 
+			printf ("{\"mac_adress\":\"%s\",", escape_colon(mac_adress)) 
+			printf ("\"local_time\":\"%s\",", escape_colon(strftime("%FT%T%z", systime())))
+		}
 		function ltrim(s) { sub(/^[ \t\r\n]+/, "", s); return s }
 		function rtrim(s) { sub(/[ \t\r\n]+$/, "", s); return s }
-		function trim(s)  { return rtrim(ltrim(s)); }
+		function trim(s)  { return tolower(rtrim(ltrim(s))); }
 		{
-			gsub(/ +/, "_", $1)
+			gsub(/ +/, "_", $1)	# replace " " with "_"
+			gsub(/\(|\)/, "", $1)	# replace "(" and ")" with nothing
 			if ( NR != anzahl ) {
 				printf ("\"%s\":%s,", trim($1), trim($2))
 			} else {
 				printf ("\"%s\":%s",  trim($1), trim($2))
 			}
 		}
-		END { printf ("%s", "}") } 
-	'
+		END { printf ("}") } 
+	' 
 }
 
 inotifywait -m $RECS_DIR -e create |
@@ -34,6 +45,6 @@ inotifywait -m $RECS_DIR -e create |
 		#echo "sox $DIR$FILE -n stat"
 
 		PLAYGROUND=`sox2json "$DIR$FILE" -n stat | tr -d '\n' `
-		mosquitto_pub -h 49.12.4.122 -t "birdnet_pi_cl/1c:69:7a:64:82:c7/sox-n" -m "$PLAYGROUND"
+		mosquitto_pub -h $MQTT_BROKER -t "$TOPIC_PRE/health/json_v2/$MAC_ADRESS/sox-n" -m "$PLAYGROUND"
 	fi
     done
