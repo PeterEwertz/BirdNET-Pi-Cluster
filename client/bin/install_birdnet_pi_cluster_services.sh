1867b56088ba62acfa5db764ea83e6b9230bcb7e
Peter Ewertz <P.Ewertz@gmx.de>
2023-02-28 13:29:22 +0100

    source list einzeilig

--- client/bin/install_birdnet_pi_cluster_services.sh
+++ client/bin/install_birdnet_pi_cluster_services.sh
@@ -24,8 +24,7 @@ install_telegraf() {
   echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | \
 	  sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | \
 	  sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-  echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] \
-	  https://repos.influxdata.com/debian stable main' | \
+  echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | \
 	  sudo tee /etc/apt/sources.list.d/influxdata.list
 
   apt install -y telegraf
