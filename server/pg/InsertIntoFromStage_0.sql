20a85defeb509c464ea5591c648978d94e56eec6
Peter Ewertz <P.Ewertz@gmx.de>
2023-08-08 17:35:35 +0200

    Staging 1.0, alte telegraf conf entfernen

---
+++ server/pg/InsertIntoFromStage_0.sql
@@ -0,0 +1,11 @@
+-- todo nach pg_cron portieren
+INSERT INTO public.birds 
+	(time, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, delay)
+	(
+	SELECT dt::timestamp, comname, host, sciname, topic, confidence, 
+		cutoff, lat, lng, overlap, sens, week, (time + interval '2h') - dt::timestamp 
+	FROM stage_0.birds s
+	WHERE (s.time, s.host) NOT IN (SELECT p.time, p.host FROM public.birds p)
+	)
+;
+
