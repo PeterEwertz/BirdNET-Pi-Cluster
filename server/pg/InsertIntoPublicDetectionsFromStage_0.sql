3be81d1b1ddb0fc9c3521c1a60261c51b5638bca
Peter Ewertz <P.Ewertz@gmx.de>
2023-08-09 20:37:53 +0200

    so wird's

--- server/pg/InsertIntoPublicDetectionsFromStage_0.sql
+++ server/pg/InsertIntoPublicDetectionsFromStage_0.sql
@@ -1,11 +1,10 @@
 -- todo nach pg_cron portieren
-INSERT INTO public.detections 
-	(time, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time_arrival)
-	(
-	SELECT dt::timestamp, comname, host, sciname, topic, confidence, 
-		cutoff, lat, lng, overlap, sens, week, time
-	FROM stage_0.birds s
-	WHERE (s.time, s.host) NOT IN (SELECT p.time, p.host FROM public.detections p)
-	)
-;
 
+
+with not_exists as (
+	select  dt::timestamp, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time from stage_0.birds st 
+	where not exists (select time, host from public.detections p where p.time = st.dt::timestamp and p.host = st.host)
+)
+insert into public.detections (time, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time_arrival)
+(select dt::timestamp, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time from not_exists)
+;
