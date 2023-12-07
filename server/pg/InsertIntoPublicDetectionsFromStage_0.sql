6255c832778381902e2276ec85f3b03b9511b5ff
PeterEwertz <p.ewertz@gmx.de>
2023-12-07 12:50:21 +0000

    aktuelle Version

--- server/pg/InsertIntoPublicDetectionsFromStage_0.sql
+++ server/pg/InsertIntoPublicDetectionsFromStage_0.sql
@@ -5,6 +5,6 @@ with not_exists as (
 	select  dt::timestamp, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time from stage_0.birds st 
 	where not exists (select time, host from public.detections p where p.time = st.dt::timestamp and p.host = st.host)
 )
-insert into public.detections (time, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time_arrival)
+insert into public.detections (time, com_name, host, sci_name, topic, confidence, cutoff, lat, lng, overlap, sens, week, time_arrival)
 (select dt::timestamp, comname, host, sciname, topic, confidence, cutoff, lat, lng, overlap, sens, week, time from not_exists)
 ;
